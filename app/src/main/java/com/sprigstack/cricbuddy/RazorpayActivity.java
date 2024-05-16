304a0722be867a7ec75acd601038172a1ee18a65
Vivek Shah <vivek.shah@zymr.com>
2024-05-16 18:11:24 +0530

    updated rzp related code

--- app/src/main/java/com/sprigstack/cricbuddy/RazorpayActivity.java
+++ app/src/main/java/com/sprigstack/cricbuddy/RazorpayActivity.java
@@ -1,30 +1,21 @@
 package com.sprigstack.cricbuddy;
 
-import static android.content.Intent.getIntent;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
-
 import com.razorpay.Checkout;
 import com.razorpay.ExternalWalletListener;
 import com.razorpay.PaymentData;
 import com.razorpay.PaymentResultWithDataListener;
-
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.util.HashMap;
@@ -34,6 +25,7 @@ public class RazorpayActivity extends AppCompatActivity implements PaymentResult
 
     private static final String TAG = RazorpayActivity.class.getSimpleName();
     private AlertDialog.Builder alertDialogBuilder;
+    private String orderId;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -55,6 +47,7 @@ public class RazorpayActivity extends AppCompatActivity implements PaymentResult
                 payBtn.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
+                        orderId = jsonObject.optString("order_id");
                         startPayment(jsonObject);
                     }
                 });
@@ -92,37 +85,11 @@ public class RazorpayActivity extends AppCompatActivity implements PaymentResult
         return queryParams;
     }
     public void startPayment(JSONObject options) {
-        /*
-          You need to pass current activity in order to let Razorpay create CheckoutActivity
-         */
         final Activity activity = this;
-
         final Checkout co = new Checkout();
         co.setKeyID("rzp_live_oQjEDMyUkDJRzj");
-
             try {
-                options.remove("key");
-                options.put("amount", "100");
-                options.put("image", "https://s3.amazonaws.com/rzp-mobile/images/rzp.png");
-                options.remove("order_id");
                 Log.d("jsonObject=====", options.toString(4));
-
-//                JSONObject options = new JSONObject();
-//                options.put("name", "Razorpay Corp");
-//                options.put("description", "Demoing Charges");
-//                options.put("send_sms_hash",true);
-//                options.put("allow_rotation", true);
-//                //You can omit the image option to fetch the image from dashboard
-//                options.put("image", "https://s3.amazonaws.com/rzp-mobile/images/rzp.png");
-//                options.put("currency", "INR");
-//                options.put("amount", "100");
-//
-//                JSONObject preFill = new JSONObject();
-//                preFill.put("email", "v7shah@gmail.com");
-//                preFill.put("contact", "9687029992");
-//
-//                options.put("prefill", preFill);
-
                 co.open(activity, options);
             } catch (Exception e) {
                 Toast.makeText(activity, "Error in payment: " + e.getMessage(), Toast.LENGTH_SHORT)
@@ -147,9 +114,8 @@ public class RazorpayActivity extends AppCompatActivity implements PaymentResult
         try{
 //            alertDialogBuilder.setMessage("Payment Successful :\nPayment ID: "+s+"\nPayment Data: "+paymentData.getData());
 //            alertDialogBuilder.show();
-
-            String successUrl = "https://dev.cricbuddy.in/payment/" + s; // Change this to your success URL
-
+            String successUrl = "https://dev.cricbuddy.in/payment/" + orderId;
+            Log.d("successUrl=====", successUrl);
             Intent intent = new Intent();
             intent.putExtra("url", successUrl);
             setResult(Activity.RESULT_OK, intent);
@@ -162,8 +128,13 @@ public class RazorpayActivity extends AppCompatActivity implements PaymentResult
     @Override
     public void onPaymentError(int i, String s, PaymentData paymentData) {
         try{
-            alertDialogBuilder.setMessage("Payment Failed:\nPayment Data: "+paymentData.getData());
-            alertDialogBuilder.show();
+//            alertDialogBuilder.setMessage("Payment Failed:\nPayment Data: "+paymentData.getData());
+//            alertDialogBuilder.show();
+            String successUrl = "https://dev.cricbuddy.in/payment/" + orderId;
+            Intent intent = new Intent();
+            intent.putExtra("url", successUrl);
+            setResult(Activity.RESULT_OK, intent);
+            finish();
         }catch (Exception e){
             e.printStackTrace();
         }
